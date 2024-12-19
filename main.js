380701877fe338a3277b419c7aaaa9460286452b
Dhener Trinidad <decreogithub@gmail.com>
2024-12-19 20:16:06 +0800

    created as separate js file and added exception for states
    * Moved most of the js to main.js
    * added exception for states of USA and Canada and change the way to convert

---
+++ main.js
@@ -0,0 +1,60 @@
+const resultContent = document.getElementById('result');
+
+const States = new Set([ // Use a Set for efficient lookups
+    "al", "ak", "az", "ar", "ca", "co", "ct", "de", "fl", "ga",
+    "hi", "id", "il", "in", "ia", "ks", "ky", "la", "me", "md",
+    "ma", "mi", "mn", "ms", "mo", "mt", "ne", "nv", "nh", "nj",
+    "nm", "ny", "nc", "nd", "oh", "ok", "or", "pa", "ri", "sc",
+    "sd", "tn", "tx", "ut", "vt", "va", "wa", "wv", "wi", "wy",
+    "AB", "BC", "MB", "NB", "NL", "NS", "NT", "NU", "ON", "PE",
+    "QC", "SK", "YT"
+]);
+
+const threeLetters = new Set([ // Use a Set for efficient lookups
+    "usa", "llc"
+]);
+
+function clear1(){
+    const test = "";
+    resultContent.innerHTML = test;
+    document.getElementById("words").value = test;
+}
+
+function titleCase(){
+    const str = document.getElementById("words").value;
+    const words = str.toLowerCase().split(" ");
+   
+    const titleCasedWords = words.map(word => {
+        if (States.has(word) && word.length === 2) { //Check for length to avoid accidental upper case
+            //console.log(word)
+            return word.toUpperCase();
+        } else if (threeLetters.has(word) && word.length === 3) {
+            //console.log(word)
+            return word.toUpperCase();
+        } else {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        }
+    });
+    
+    resultContent.innerHTML = titleCasedWords.join(" ");
+};
+
+function copyToClipboard(text) {
+    if (navigator.clipboard && window.isSecureContext) {
+        return navigator.clipboard.writeText(text);
+        
+    } else {
+        // Fallback for older browsers (see method 2 below)
+        return fallbackCopyToClipboard(text);
+    }
+}
+
+document.getElementById("copy_result").addEventListener('click', async () => {
+    try {
+        const text = await navigator.clipboard.readText();
+        alert(`Result Copied: ${text}`);
+    } catch (err) {
+        console.error('Failed to read clipboard: ', err);
+        alert("Failed to read clipboard");
+    }    
+});
